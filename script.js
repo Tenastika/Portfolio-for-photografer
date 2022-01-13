931566c1aedb084f11c3c4402eba1075a97ffe8c
Anastasia Terenteva <tenastika@mail.ru>
2022-01-13 21:07:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict'
+/* Активация бургер-меню */
+const navMenu = document.querySelector('.header__nav')
+const menuBtn = document.querySelector(".hamburger");
+
+menuBtn.addEventListener("click", function(e) {
+  
+  menuBtn.classList.toggle("is-active");
+  navMenu.classList.toggle('active-menu'); 
+  e.stopPropagation();
+});
+
+document.addEventListener ('click', function(e) {
+  const navList = document.querySelector('.header__nav-list')
+    if (!(navList.contains(e.target)) && 
+        (navMenu.classList.contains('active-menu'))) {
+
+        menuBtn.classList.toggle("is-active");
+        navMenu.classList.toggle('active-menu');     
+    }
+})
+
+document.addEventListener ('click', function(e) {
+	if ( (e.target.classList.contains('header__nav-item-link')) && 
+			(navMenu.classList.contains('active-menu'))) {
+
+			menuBtn.classList.toggle("is-active");
+			navMenu.classList.toggle('active-menu');     
+	}
+})
