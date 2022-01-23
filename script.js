b5ea52ec5f900c939282e13773a4b79ade3d065b
Anastasia Terenteva <tenastika@mail.ru>
2022-01-23 17:40:59 +0300

    feat: add change photo

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/*====================гамбургер меню=======================*/
+
 const navMenu = document.querySelector('.header__nav')
 const menuBtn = document.querySelector(".hamburger");
 
@@ -32,3 +34,24 @@ document.addEventListener ('click', function(e) {
       document.body.classList.toggle('overflow');      
 	}
 })
+
+/*==================переключение фото в секции портфолио======================*/
+
+
+const arrBtns = document.querySelectorAll('.portfolio__buttons-btn'),
+      portfolioBtns =  document.querySelector('.portfolio__buttons'),     
+      portfolioPhotos = document.querySelectorAll('.portfolio__img');
+
+portfolioBtns.addEventListener('click', function changePhoto (e) {
+  if (e.target.classList.contains('portfolio__buttons-btn')) {
+    let btn = e.target;
+   
+    arrBtns.forEach(btn => btn.classList.remove('_active'));
+    btn.classList.add('_active');
+    portfolioPhotos.forEach((img, i) => {
+       img.src = `./assets/img/portfolio/${btn.dataset.season}/${i + 1}.jpg`
+     });  
+  }
+})
+
+
